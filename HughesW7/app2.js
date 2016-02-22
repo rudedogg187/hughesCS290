88a4e5f1e41987b1a9eb187a3e975da322ccf2ea
rudedogg187 <hughejos@oregonstate.edu>
2016-02-21 16:46:41 -0800

    console mod

--- HughesW7/app2.js
+++ HughesW7/app2.js
@@ -21,7 +21,8 @@ app.get('/', function(request, response) {
 //GET LOOPBACK
 app.get('/get-loopback', function(request, response) {
 	var qParams = getParams(request.query);
-	console.log('GET QUERY: ' + qParams.name + ' : ' + qParams.value);
+	console.log('GET QUERY:');
+	console.log(qParams);
 	var context = {};
 	context.dataLst = qParams;
 	response.render('get-loopback.handlebars', context);
@@ -37,8 +38,10 @@ app.post('/post-loopback', function(request, response) {
 	}*/
 	qParams = getParams(request.query);
 	bParams = getParams(request.body);
-	console.log('POST QUERY: ' + qParams.name + ' : ' + qParams.value);
-	console.log('POST BODY: ' + bParams.name + ' : ' = bParams.value);
+	console.log('POST QUERY:');
+	console.log(qParams);
+	console.log('POST BODY:');
+	console.log(bParams);
 	var context = {};
 	context.dataLst = bParams;
 	response.render('post-loopback', context);
